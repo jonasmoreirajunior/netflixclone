9e0fb6e80d0d38013c11bd9370fbe98fa09b66df
Jonas Moreira <jonas.om@outlook.com>
2021-10-18 12:16:12 -0300

    section featured movie

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null);
 
   useEffect(()=>{
     const loadAll = async () => {
+      //Pega lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pega Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
     }
 
     loadAll();
@@ -18,6 +28,11 @@ export default () => {
 
   return (
     <div className='page'>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className='lists'>
         {movieList.map((item, key)=>(
           <MovieRow key={key} title={item.title} items={item.items} />
